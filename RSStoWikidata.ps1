11bd68c703d587cbbef98131e52d1becd2fea745
Neil Carmichael <back_ache@hotmail.com>
2023-08-30 16:56:22 +0100

    Add files via upload

--- RSStoWikidata.ps1
+++ RSStoWikidata.ps1
@@ -1,48 +1,152 @@
-# Get Atom Feed
-$Response = Invoke-WebRequest -Uri "https://feeds.megaphone.fm/LAV7402407500" -UseBasicParsing -ContentType "application/xml"
-If ($Response.StatusCode -ne "200") {
-    # Feed failed to respond.
-    Write-Host "Message: $($Response.StatusCode) $($Response.StatusDescription)"
+#Get  URL
+$Q = "Q110568256","Q68131854","Q110568719","Q110568737","Q110573808","Q110573857","Q110573969","Q110573979","Q110573992","Q110573998"
+#to do
+# get the podcast logo URL and put it in P10286
+# take <itunes:category text="Education"> and set it as genre
+# get <link> and make it official website
+function Send-Wikidata {
+
+    param (
+        $QID
+    )
+    $out =""
+    $language = @{
+        'en'    = 'Q1860'
+        'de'    = 'Q188'
+        'de-de'    = 'Q188'
+        'fr'    = 'Q150'
+        'es'    = 'Q1321'
+        'en-us' = 'Q7976'
+        'en-ph' = 'Q1413694'
+        'en-gb' = 'Q7979'
+        'pl'    = 'Q809'
+        'nl'    = 'Q7411' 
+        'pt-br' = 'Q750553'
+        'pt' = 'Q5146'
+        'en-au' ='Q44679'
+        'en-ca' = 'Q44676'
+        'en_US' = 'Q7976'
+        'es-mx' = 'Q616620'
+        'es_mx' = 'Q616620'
+        'es-ES'= 'Q1321'
+    }
+
+    #Retrieve the wikidata item and put in a variable called WD then show me what you got
+    $Response = Invoke-WebRequest -Uri "https://wikidata.org/w/rest.php/wikibase/v0/entities/items/$QID" -UseBasicParsing
+    If ($Response.StatusCode -ne "200") {
+        # WD failed to respond.
+        Write-Host "Message: $($Response.StatusCode) $($Response.StatusDescription)"
+    }
+
+    #Make a dictionary of the response from Wikidata
+    $WD = $response | ConvertFrom-Json
+    #$WD
+    #if more than one $URL=$WD.statements.p1019.value.content[1]
+    #if just one $URL=$WD.statements.p1019.value.content
+   
+    #From the WIkidate Dictionary  extra the value of the web feed URL
+    $URL = $WD.statements.p1019.value.content | Select-Object -first 1
+
+    # Go and get the RSS file
+    try {
+        $Response = Invoke-WebRequest -Uri "$URL" -UseBasicParsing -ContentType "application/xml"
+
+        $FeedXml = [xml]$Response.Content
+        #$Entries = @()
+        #$Now = Get-Date
+
+        # Exract recent entries (currently set for updated within the last 24 hours)
+        ForEach ($Entry in $FeedXml.feed.entry) {
+
+           $URL=$Entry 
+        }
+
+        $chfeed = [xml](Invoke-WebRequest "$URL")
+        #$chfeed.rss.channel.item | Select-Object pubDate
+        $countitems = 0
+        [datetime] $oldestdate = "1972-08-14"
+        [datetime] $newestdate = "1972-08-14"
+
+        ForEach ($Entry in $chfeed.rss.channel.item) {
+
+            $found = $Entry.pubDate -match '(\d\d \w\w\w\ \d\d\d\d)'
+            #$found
+            if ($found) {
+                $epdate = $matches[1]
+            }
+            [datetime] $date = [datetime] $epdate
+            if ($oldestdate = "1972-08-14") { $oldestdate = $date }
+            #if ($newestdate="1972-08-14") {$newestdate=$date}
+            if ($date -lt $oldestdate) { $oldestdate = $date }
+            if ($date -gt $newestdate) { $newestdate = $date }
+            $countitems += 1
+        }
+#Write-Output "For podcast $QID $countitems episodes found"
+        #$title=$chfeed.rss.channel.title.replace("|","%7C")
+        $date = Get-Date
+        $datestr = $date.ToString("yyyy-MM-dd")
+        #get the largest number of podcasts the wididata entry has
+        $amountnum=0
+        $currentcount=0
+        ForEach ($CountEntry in $WD.statements.p1113.value.content.amount) {
+    
+            if ($CountEntry.length -gt 0) {$currentcount = $CountEntry.Substring(1)}
+            if ($currentcount -gt $amountnum) {$amountnum=$currentcount} 
+         }
+        if ($countitems -gt $amountnum) {$out = "%7C%7C$QID%7CP1113%7C$countitems%7CP585%7C%2b$($datestr)T00:00:00Z%2F11"}
+        #Start-Process "chrome.exe" "$output"
+        #Write-output $output
+        if ($WD.statements.p407.value.content.length -eq 0){
+        $lang = $chfeed.rss.channel.language.innertext
+        #$lang.length
+        if ($lang.length -eq 0) {$lang = $chfeed.rss.channel.language}
+        #Write-Output "Its raw language is $lang"
+        $lang = $language[$lang.ToLower()]
+        #$opensquare = $lang.IndexOf("[CDATA[")
+        #$closesquare = $lang.IndexOf("]")
+        #if ($opensquare -ne 0) {
+        #    $opensquare += 7
+        #    #$len=$lang.length
+        #    $numtotake = $closesquare - $opensquare
+        #    $lang = $lang.substring($opensquare, $numtotake)
+        #}
+        $out += "%7C%7C$QID%7CP407%7C$($lang)"
+        }
+        #$chfeed.rss.channel.language
+        #Start-Process "chrome.exe" "$output"
+        #Write-output $out
+
+        #$title = [uri]::EscapeDataString($chfeed.rss.channel.title)
+        #$output="https://quickstatements.toolforge.org/#/v1=%7C%7C$QID%7CP1476%7C$($title)"
+        #Write-output $output
+
+        #$QID+"|P1476|"+$title+"|P580|"+$oldestdate+"|P1113|"+$countitems+"|S585|"+$date
+
+        #$oldestdate
+        #$newestdate
+
+        #$chfeed.rss.channel.subtitle
+        #If it doesn't have a home page get one from the feed
+        #Write-Output "For podcast $QID WD has *$($WD.statements.p856.value.content.length)* and the feed has $($chfeed.rss.channel.link| Where-Object { $_.rel.length -eq 0})"
+
+        if ($WD.statements.p856.value.content.length -eq 0){
+         #   Write-Output "set it"
+
+            $homepage=$chfeed.rss.channel.link| Where-Object { $_.rel.length -eq 0}
+            $homepage=[System.Web.HTTPUtility]::UrlEncode($homepage)
+            $out += "%7C%7C$QID%7CP856%7C""$homepage"""
+        }
+        
+    }
+    catch {} 
+    return $out
 }
 
-$FeedXml = [xml]$Response.Content
-$Entries = @()
-$Now = Get-Date
+$output = "https://quickstatements.toolforge.org/#/v1="
 
-# Exract recent entries (currently set for updated within the last 24 hours)
-ForEach ($Entry in $FeedXml.feed.entry) {
-
-    $Entry 
+foreach ($Qitem in $Q) {
+    $itemoutput = Send-Wikidata($Qitem)
+    $output = $output + $itemoutput
 }
-
-
-$QID="Q79675897"
-$chfeed = [xml](Invoke-WebRequest "https://feeds.megaphone.fm/LAV7402407500")
-$chfeed.rss.channel.item | Select-Object pubDate
-$countitems = 0
-[datetime] $oldestdate="1972-08-14"
-[datetime] $newestdate="1972-08-14"
-
-ForEach ($Entry in $chfeed.rss.channel.item) {
-
-    [datetime] $date=[datetime] $Entry.pubDate
-    if ($oldestdate="1972-08-14") {$oldestdate=$date}
-    #if ($newestdate="1972-08-14") {$newestdate=$date}
-    if ($date -lt $oldestdate) {$oldestdate=$date}
-    if ($date -gt $newestdate) {$newestdate=$date}
-    $countitems += 1
-}
-
-$title=$chfeed.rss.channel.title.replace("|","%7C")
-$date=Get-Date
-"QID,P1476,P580,P1113,S585"
-$QID+","""+$title+""","""+$oldestdate+""","""+$countitems+""""+$date+""""
-
-#$QID+"|P1476|"+$title+"|P580|"+$oldestdate+"|P1113|"+$countitems+"|S585|"+$date
-
-#$countitems
-#$countitems
-#$oldestdate
-#$newestdate
-
-#$chfeed.rss.channel.subtitle
+$output
+#   Start-Process "chrome.exe" "$output"
